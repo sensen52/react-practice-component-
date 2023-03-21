22c2c9e39b72135732dd53128595569ddbbedcad
jungsein <gytls52tpdls@naver.com>
2023-03-21 18:36:04 +0900

    add map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import Movie from "./components/Movie.js";
+import poster1 from "./images/poster1.jpg";
+import poster2 from "./images/poster2.jpg";
+import poster3 from "./images/poster3.jpg";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: [
+        {
+          image: poster1,
+          name: "빌리엘리어트",
+          rating: "9.5⭐",
+          date: "2023년 10월2일",
+        },
+        {
+          image: poster2,
+          name: "원더풀라이프",
+          rating: "8.3⭐",
+          date: "2022년 2월4일",
+        },
+        {
+          image: poster3,
+          name: "시네마천국",
+          rating: "10⭐",
+          date: "1927년 3월 7일",
+        },
+      ],
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const test = this.state.movieList.map((data,index) => <Movie
+      key = {index}
+      index = {index}
+      image={data.image}
+      name={data.name}
+      rating={data.rating}
+      date={data.date}
+    />);
+
+    return (
+      <div className="App">
+        <h1>🎥BEST MOVIE🎥</h1>
+        <ul className="flex_comp">
+          <li>
+            {test}
+            {/* <Movie
+          image={this.state.movieList[0].image}
+          name={this.state.movieList[0].name}
+          rating={this.state.movieList[0].rating}
+          date={this.state.movieList[0].date}
+          
+        />
+          </li>
+          <li>
+          <Movie
+          image={this.state.movieList[1].image}
+          name={this.state.movieList[1].name}
+          rating={this.state.movieList[1].rating}
+          date={this.state.movieList[1].date}
+        />
+          </li>
+          <li>
+          <Movie
+          image={this.state.movieList[2].image}
+          name={this.state.movieList[2].name}
+          rating={this.state.movieList[2].rating}
+          date={this.state.movieList[2].date}
+        /> */}
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import { useEffect , useState } from "react";
+import "../css/movie.css";
+
+function Movie(props) {
+  const change = () => {
+    let txt = document.querySelector(".title").textContent;
+    console.log(txt);
+  }
+
+  return (
+    <div className="movie">
+      <ul>
+        <li onClick={change}>
+          <img src={props.image} />
+        </li>
+        <li className="title">{props.name}</li>
+        <li>{props.rating}</li>
+        <li>{props.date}</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Movie;
